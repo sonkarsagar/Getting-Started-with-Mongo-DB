658fa155c465699616ebc0cdc56d95ab840bdd11
Sagar SOnkar <sagarsonker56625@gmail.com>
2023-08-15 03:33:41 +0530

    Introduction to Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect
 const User = require('./models/user')
 
 const app = express();
@@ -18,21 +18,22 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('64da265a426d24011e82268b')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('64da265a426d24011e82268b')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-
-  app.listen(3000);
+mongoose.connect('mongodb+srv://sagarsonker56625:vyk50nF8a31h8jvO@cluster0.xpyytnw.mongodb.net/shop?retryWrites=true&w=majority').then((result) => {
+  app.listen(3000)
+}).catch((err) => {
+  console.log(err);
 });
